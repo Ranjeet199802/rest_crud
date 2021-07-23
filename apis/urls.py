3b798e43d51c9d20af96c77b2daf57f30b7be2a1
Ranjeet199802 <ranjeetgup19@gmail.com>
2021-07-23 17:44:32 +0530

    class based views

--- apis/urls.py
+++ apis/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from apis import views
 
 urlpatterns = [
-    path('create/', views.create_student),
-    path('get_data/<int:pk>', views.get_data_by_id),
-    path('get_data/', views.get_all_data),
+    path('create/', views.CreateStudent.as_view()),
+    path('get_student/', views.GetStudent.as_view()),
+    path('update_student/<int:pk>', views.UpdateStudent.as_view()),
+    path('delete_student/<int:pk>', views.DeleteStudent.as_view()),
+    # path('get_data/<int:pk>', views.get_data_by_id),
+    # path('get_data/', views.get_all_data),
 ]
--- apis/views.py
+++ apis/views.py
@@ -2,96 +2,154 @@ import io
 
 from django.http.response import JsonResponse
 from django.shortcuts import render
-
-# Create your views here.
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
+from django.utils.decorators import method_decorator
+from django.views import View
 from .models import Student
 
 from apis.serializers import StudentSerializer
 
 
-@csrf_exempt
-def create_student(request):
-    if request.method == 'POST':
-        json_data = request.body
-        stream = io.BytesIO(json_data)
-        value = JSONParser().parse(stream)
-        serializer = StudentSerializer(data=value)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse("successfully created data", status=201, safe=False)
-        else:
-            return JsonResponse("something went wrong", safe=False)
+@method_decorator(csrf_exempt, name='dispatch')
+class CreateStudent(View):
+    """
+    class based api view for create student using serializer
+    """
 
-
-@csrf_exempt
-def get_data_by_id(request, pk):                        # aoi for get single user details
-    try:
-        data = Student.objects.get(id=pk)
-    except Exception as e:
-        return JsonResponse(
-            {
-                "message": "id not exists"
-            }
-        )
-
-    if request.method == 'GET':
-
-        if data:
-            serializer = StudentSerializer(data, many=False)
-            return JsonResponse(serializer.data, safe=False)
-
-    elif request.method == 'PATCH':                            # api for updations
-        if data:
-            req_data = request.body
-            stream = io.BytesIO(req_data)
+    def post(self, request, *args, **kwargs):
+        try:
+            json_data = request.body
+            stream = io.BytesIO(json_data)
             value = JSONParser().parse(stream)
-
             if value:
-
-                serializer = StudentSerializer(data, data=value, partial=True)
-
+                serializer = StudentSerializer(data=value)
                 if serializer.is_valid():
                     serializer.save()
-                    return JsonResponse(
-                        {
-                            "message": "data updated successfully"
-                        }
-                    )
+                    return JsonResponse("successfully created data", status=201, safe=False)
                 else:
-                    return JsonResponse(
-                        {
-                            "message": "something went wrong"
-                        }
-                    )
+                    return JsonResponse("something went wrong", safe=False)
+            else:
+                return JsonResponse(
+                    {
+                        "message": "didnot get value"
+                    }
+                )
+        except Exception as e:
+            return JsonResponse(
+                {
+                    "message": "something went wrong"
+                },
+                status=500
+            )
 
+
+@method_decorator(csrf_exempt, name='dispatch')
+class GetStudent(View):
+    """
+    class based api view for getting student details using serializer
+"""
+
+    def get(self, request):
+        id = request.GET.get('id')
+
+        if id:
+            try:
+                data = Student.objects.get(id=id)
+            except Exception as e:
+                data = None
+            if data:
+                serializer = StudentSerializer(data, many=False)
+                return JsonResponse(serializer.data, status=202)
             else:
                 return JsonResponse(
                     {
                         "message": "data not found"
-                    }
+                    }, status=404
+
                 )
 
-    elif request.method == 'DELETE':        #api for delete data by id
+        else:
+
+            data = Student.objects.all()
+            if data:
+                serializer = StudentSerializer(data, many=True)
+                return JsonResponse(serializer.data, safe=False)
+            else:
+                return JsonResponse(
+                    {
+                        "message": "data not found"
+                    },
+                    status=404
+                )
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class UpdateStudent(View):
+    """
+    class based api view for updating student details paritally using serializer
+"""
+
+    def patch(self, request, pk):
+        try:
+            data = Student.objects.get(id=pk)
+        except Exception as e:
+            return JsonResponse(
+                {
+                    "message": "data not found"
+                },
+                status=404
+            )
+
+        if data:
+            req_data = request.body
+            stream = io.BytesIO(req_data)
+            parsed_data = JSONParser().parse(stream)
+            serializer = StudentSerializer(data, data=parsed_data, partial=True)
+
+            if serializer.is_valid():
+                serializer.save()
+                return JsonResponse(
+                    {
+                        "message": "data updated successfully"
+                    },
+                    status=201
+                )
+            else:
+                return JsonResponse(
+                    {
+                        "message": "data not updated"
+                    },
+                    status=404
+                )
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class DeleteStudent(View):
+    """
+        class based api view for deleting student record
+    """
+
+    def delete(self, request, pk):
+        try:
+            data = Student.objects.get(id=pk)
+        except Exception as e:
+            data = None
+
         if data:
             data.delete()
             return JsonResponse(
                 {
-                    "message": "data deleted successfully"
-                }
+                    "message": "data deletedd successfully"
+                },
+                status=200
             )
 
         else:
             return JsonResponse(
                 {
-                    "message": "something went wrong"
-                }
+                    "message": "data not found"
+                },
+                status=404
             )
-
-
-def get_all_data(request):
-    if request.method == 'GET':  # api to get all users
-        data = Student.objects.all()
-        serializer = StudentSerializer(data, many=True)
-        return JsonResponse(serializer.data, safe=False)
+#
